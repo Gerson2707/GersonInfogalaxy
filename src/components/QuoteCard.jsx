5eeebfe969dc0a3a782503d299c5252eff75d513
GERSON <gersonllamas@hotmail.com>
2023-09-29 16:17:59 -0600

    lanzamiento

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,26 @@
+import "./QuoteCard.css"
+
+const QuoteCard = ({ quote,handleChangeQuote }) => {
+
+
+  return (
+    <section className="quoteCard">
+      <article className="quoteCard__container">
+        <h1 className="quoteCard__title">Infogalaxi</h1>
+
+        <p className="quoteCard__phrase">{quote.phrase}</p>
+
+        <button className="quoteCard__btn" onClick={handleChangeQuote}>
+          Change
+        </button>
+      </article>
+
+      <footer className="quoteCard__footer">
+        Author: {quote.author}
+      </footer>
+    </section>
+  );
+};
+export default QuoteCard;
+
+// min 1:44
